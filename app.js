e00c4d2816688c11458d3794f212e76fd74cdbc9
Lucky <luckych8080@gmail.com>
2021-10-19 03:44:39 +0530

    Created login, registration page, all product page, navbar, footer, boilerplate

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require('express');
-const app = express();
 const port = 8080;
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', function (req, res) {
     res.send('hello world')
 })
 
+app.get('/collections/all-products', (req, res) => {
+    res.render('collections/allproducts');
+})
 
-app.listen(port, ()=>{
+
+
+
+
+
+
+app.get('/login', (req, res)=>{
+    res.render('users/login')
+})
+
+app.post('/login', (req, res)=>{
+    res.render('users/login')
+})
+
+
+app.get('/register', (req, res)=>{
+    res.render('users/register')
+})
+
+app.listen(port, () => {
     console.log(`Serving on localhost:${port}`)
 })
