b335fc4b236ce7444e023a9ac6616099f7e493c1
Lucky <luckych8080@gmail.com>
2021-10-20 01:53:37 +0530

    client side validation, catchAsync, error page, route folder

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Product = require('./models/product')
 const ejsMate = require('ejs-mate');
+const productsRoutes = require('./routes/products');
+
+const ExpressError = require('../YelpCamp/utils/ExpressError');
 
 const dbUrl = 'mongodb://localhost:27017/anythingsell';
 mongoose.connect(dbUrl);
@@ -30,43 +33,7 @@ app.get('/', function (req, res) {
     res.render('home');
 })
 
-app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', {products});
-})
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new');
-})
-
-app.post('/products', async (req, res) => {
-    const product = new Product(req.body.product);
-    console.log(product);
-    await product.save();
-    res.redirect(`/products/${product._id}`)
-})
-
-app.get('/products/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    res.render('products/show', { product });
-});
-
-app.get('/products/:id/edit', async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    res.render('products/edit', { product });
-})
-
-app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, { ...req.body.product });
-    res.redirect(`/products/${product._id}`)
-});
-
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
-})
 
 // app.get('/login', (req, res)=>{
 //     res.render('users/login')
@@ -80,6 +47,19 @@ app.delete('/products/:id', async (req, res) => {
 // app.get('/register', (req, res)=>{
 //     res.render('users/register')
 // })
+
+app.use("/products", productsRoutes);
+
+
+app.all('*', (req, res, next)=>{
+    next(new ExpressError('Page not found', 404));
+});
+
+app.use((err, req, res, next) =>{
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = "Oh No, Something went wrong!!"
+    res.status(statusCode).render('error', {err});
+})
 
 app.listen(port, () => {
     console.log(`Serving on https://localhost:${port}`)
