8d2e016e76538c822b66b23df0b52fcbd5cd11d0
Lucky <luckych8080@gmail.com>
2021-10-26 17:23:45 +0530

    Created a Controllers dir

--- routes/products.js
+++ routes/products.js
@@ -1,68 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product')
+const Product = require('../models/product');
+const products = require('../controllers/products');
 const { productSchema } = require('../schemas.js');
-const {isLoggedIn, isAuthor, validateProduct} = require('../middleware');
+const { isLoggedIn, isAuthor, validateProduct } = require('../middleware');
 
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
 
-router.get('/', catchAsync(async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
-}))
+router.route('/')
+    .get(catchAsync(products.index))
+    .post(isLoggedIn, validateProduct, catchAsync(products.createProduct))
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('products/new');
-})
+router.get('/new', isLoggedIn, products.renderNewForm)
 
-router.post('/', isLoggedIn, validateProduct, catchAsync(async (req, res) => {
-    const product = new Product(req.body.product);
-    product.author = req.user._id;
-    console.log(product);
-    await product.save();
-    req.flash('success', 'Successfully made a new product!');
-    res.redirect(`/products/${product._id}`)
-}))
+router.route('/:id')
+    .get(catchAsync(products.showProduct))
+    .put(isLoggedIn, isAuthor, validateProduct, catchAsync(products.updateProduct))
+    .delete(isLoggedIn, isAuthor, catchAsync(products.deleteProduct))
 
-router.get('/:id',  catchAsync(async (req, res) => {
-    const product = await Product.findById(req.params.id).populate({
-        path: "reviews",
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    console.log(product)
-    if(!product){
-        req.flash('error', 'Cannot find that Product!');
-        res.redirect('/products');
-    }
-    res.render('products/show', { product });
-}));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if(!product){
-        req.flash('error', 'Cannot find that Product!')
-        res.redirect('/products');
-    }
-    res.render('products/edit', { product });
-}))
-
-router.put('/:id', isLoggedIn, isAuthor, validateProduct, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, { ...req.body.product });
-    req.flash('success', 'Successfully update Product!');
-    res.redirect(`/products/${product._id}`)
-}));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Product.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted Product')
-    res.redirect('/products');
-}))
-
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(products.renderEditForm))
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,45 +3,23 @@ const router = express.Router();
 const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
+const users = require("../controllers/users");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router.get("/register", users.renderRegisterForm);
+
+router.post("/register", catchAsync(users.createUser));
+
+router.get("/login", users.renderLoginForm);
 
 router.post(
-  "/register",
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash("success", "Welcome to Red!");
-        res.redirect("/products");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  users.loginUser
 );
 
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', 'welcome back!');
-  const redirectUrl = req.session.returnTo || '/products';
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-})
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Goodbye!");
-  res.redirect("/products");
-});
+router.get("/logout", users.logoutUser);
 
 module.exports = router;
